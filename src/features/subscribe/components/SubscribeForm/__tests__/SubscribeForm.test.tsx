bf6236fa18d095557ac71beccfedb084425f5b86
fthomasvp <fellipecab@gmail.com>
2022-07-22 03:36:30 -0300

    test: improve code readability
    - Add helper functions to reduce duplications.

--- src/features/subscribe/components/SubscribeForm/__tests__/SubscribeForm.test.tsx
+++ src/features/subscribe/components/SubscribeForm/__tests__/SubscribeForm.test.tsx
@@ -3,10 +3,48 @@ import userEvent from "@testing-library/user-event";
 
 import SubscribeForm from "../SubscribeForm";
 
-describe("SubscribeForm", () => {
-  it("should init with empty inputs", () => {
-    render(<SubscribeForm />)
+type InputNames = {
+  username: string;
+  age: string;
+  password: string;
+}
 
+describe("SubscribeForm", () => {
+  beforeEach(() => {
+    render(<SubscribeForm />)
+  });
+
+  const typeIntoInput = async ({ username, age, password }: Partial<InputNames>) => {
+    const usernameInputElement: HTMLInputElement = screen.getByRole("textbox", {name: /username/i});
+    const ageInputElement: HTMLInputElement = screen.getByLabelText(/age/i);
+    const passwordInputElement: HTMLInputElement = screen.getByLabelText(/password/i);
+
+    if (username) {
+      await userEvent.type(usernameInputElement, username);
+    }
+
+    if (age) {
+      await userEvent.type(ageInputElement, age);
+    }
+
+    if (password) {
+      await userEvent.type(passwordInputElement, password);
+    }
+
+    return {
+      usernameInputElement,
+      ageInputElement,
+      passwordInputElement,
+    }
+  }
+
+  const clickOnSubmitButton = async () => {
+    const submitButtonElement = screen.getByRole("button", {name: /submit/i})
+
+    await userEvent.click(submitButtonElement);
+  }
+
+  it("should init with empty inputs", () => {
     const usernameInputElement: HTMLInputElement = screen.getByRole("textbox", {name: /username/i});
     const ageInputElement: HTMLInputElement = screen.getByLabelText(/age/i);
     const passwordInputElement: HTMLInputElement = screen.getByLabelText(/password/i);
@@ -17,96 +55,58 @@ describe("SubscribeForm", () => {
   });
 
   it("should be able to type a username", async () => {
-    render(<SubscribeForm />)
-
-    const usernameInputElement: HTMLInputElement = screen.getByRole("textbox", {name: /username/i});
-
-    await userEvent.type(usernameInputElement, "fthomasvp");
+    const {usernameInputElement} = await typeIntoInput({username: "fthomasvp"})
 
     expect(usernameInputElement.value).toBe("fthomasvp");
   });
 
   it("should be able to type an age", async () => {
-    render(<SubscribeForm />)
-
-    const ageInputElement: HTMLInputElement = screen.getByLabelText(/age/i);
-
-    await userEvent.type(ageInputElement, "72");
+    const {ageInputElement} = await typeIntoInput({age: "72"})
 
     expect(ageInputElement.value).toBe("72");
   });
 
   it("should be able to type a password", async () => {
-    render(<SubscribeForm />)
-
-    const passwordInputElement: HTMLInputElement = screen.getByLabelText(/password/i);
-
-    await userEvent.type(passwordInputElement, "sub123");
+    const {passwordInputElement} = await typeIntoInput({password: "sub123"})
 
     expect(passwordInputElement.value).toBe("sub123");
   });
 
   it("should show username error if no username is provided", async () => {
-    render(<SubscribeForm />)
+    await clickOnSubmitButton();
 
-    const submitButtonElement = screen.getByRole("button", {name: /submit/i})
-
-    await userEvent.click(submitButtonElement);
-
-    const errorDescriptionElement = screen.queryByText(/username is required/i);
-
-    expect(errorDescriptionElement).toBeInTheDocument();
+    expect(screen.queryByText(/username is required/i)).toBeInTheDocument();
   });
 
   it("should show age error if no age is provided", async () => {
-    render(<SubscribeForm />)
+    await typeIntoInput({
+      username: "fthomasvp",
+    })
+    await clickOnSubmitButton();
 
-    const usernameInputElement: HTMLInputElement = screen.getByRole("textbox", {name: /username/i});
-    const submitButtonElement = screen.getByRole("button", {name: /submit/i})
-
-    await userEvent.type(usernameInputElement, "fthomasvp");
-    await userEvent.click(submitButtonElement);
-
-    const errorDescriptionElement = screen.queryByText(/age is required/i);
-
-    expect(errorDescriptionElement).toBeInTheDocument();
+    expect(screen.queryByText(/age is required/i)).toBeInTheDocument();
   });
 
   it("should show password error if no password is provided", async () => {
-    render(<SubscribeForm />)
+    await typeIntoInput({
+      username: "fthomasvp",
+      age: "72",
+    })
+    await clickOnSubmitButton();
 
-    const usernameInputElement: HTMLInputElement = screen.getByRole("textbox", {name: /username/i});
-    const ageInputElement: HTMLInputElement = screen.getByLabelText(/age/i);
-    const submitButtonElement = screen.getByRole("button", {name: /submit/i})
-
-    await userEvent.type(usernameInputElement, "fthomasvp");
-    await userEvent.type(ageInputElement, "72");
-    await userEvent.click(submitButtonElement);
-
-    const errorDescriptionElement = screen.queryByText(/password is required/i);
-
-    expect(errorDescriptionElement).toBeInTheDocument();
+    expect(screen.queryByText(/password is required/i)).toBeInTheDocument();
   });
 
   it("should show no error message if every input is valid", async () => {
-    render(<SubscribeForm />)
+    await typeIntoInput({
+      username: "fthomasvp",
+      age: "72",
+      password: "sub123"
+    })
+    await clickOnSubmitButton();
 
-    const usernameInputElement: HTMLInputElement = screen.getByRole("textbox", {name: /username/i});
-    const ageInputElement: HTMLInputElement = screen.getByLabelText(/age/i);
-    const passwordInputElement: HTMLInputElement = screen.getByLabelText(/password/i);
-    const submitButtonElement = screen.getByRole("button", {name: /submit/i})
-
-    await userEvent.type(usernameInputElement, "fthomasvp");
-    await userEvent.type(ageInputElement, "72");
-    await userEvent.type(passwordInputElement, "sub123");
-    await userEvent.click(submitButtonElement);
-
-    const usernameErrorDescriptionElement = screen.queryByText(/username is required/i);
-    const ageErrorDescriptionElement = screen.queryByText(/age is required/i);
-    const passwordErrorDescriptionElement = screen.queryByText(/password is required/i);
-
-    expect(usernameErrorDescriptionElement).not.toBeInTheDocument();
-    expect(ageErrorDescriptionElement).not.toBeInTheDocument();
-    expect(passwordErrorDescriptionElement).not.toBeInTheDocument();
+    expect(screen.queryByText(/username is required/i)).not.toBeInTheDocument();
+    expect(screen.queryByText(/age is required/i)).not.toBeInTheDocument();
+    expect(screen.queryByText(/password is required/i)).not.toBeInTheDocument();
   });
 });

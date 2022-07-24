3e58cc16216140dad9675b1036c4cf21292a19ab
fellipe.thomas <fellipe.thomas@bbchain.com.br>
2022-07-24 01:44:31 -0300

    feat: add tests results on Azure Pipelines

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,7 @@ export default defineConfig({
     globals: true,
     environment: "jsdom",
     setupFiles: "src/setupTests.ts",
+    outputFile: "TEST-bbchain-react-testing.xml",
     coverage: {
       reporter: ["text", "html", "cobertura"],
       exclude: [

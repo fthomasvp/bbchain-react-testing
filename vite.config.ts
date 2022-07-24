f5e1928cee13dbb42845de9d8e6d7b0ab835f700
fellipe.thomas <fellipe.thomas@bbchain.com.br>
2022-07-23 22:56:02 -0300

    test: update reporters to use on Azure Pipelines

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,7 +12,7 @@ export default defineConfig({
     environment: "jsdom",
     setupFiles: "src/setupTests.ts",
     coverage: {
-      reporter: ["text", "json", "html"],
+      reporter: ["cobertura"],
       exclude: [
         "src/setupTests.ts",
         "src/mocks/**",
@@ -20,5 +20,6 @@ export default defineConfig({
         "src/utils/test-utils.tsx",
       ],
     },
+    reporters: ["junit"],
   },
-})
+});

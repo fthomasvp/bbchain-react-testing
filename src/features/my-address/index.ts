db275f8a009761f73d0e0343e75bbe8190113f41
fthomasvp <fellipecab@gmail.com>
2022-07-28 22:29:43 -0300

    refactor: add type on import declaration

--- src/features/my-address/index.ts
+++ src/features/my-address/index.ts
@@ -1,7 +1,7 @@
 import MyAddressForm from "./components/my-address-form/MyAddressForm";
 import MyAddressDetails from "./components/my-address-details/MyAddressDetails";
 import { getZipCodeService } from "./services";
-import { Address } from "./types";
+import type { Address } from "./types";
 
 export { MyAddressForm, MyAddressDetails, getZipCodeService };
 export type { Address };
